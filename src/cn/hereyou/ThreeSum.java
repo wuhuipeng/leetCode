44408280f0ec160fcb589bc1e994c1cb089286f3
wuhuipeng <2252436103@qq.com>
2023-04-15 16:16:56 +0800

    16.最接近三数之和

---
+++ src/cn/hereyou/ThreeSum.java
@@ -0,0 +1,42 @@
+package cn.hereyou;
+
+
+import java.util.Arrays;
+
+/**
+ * 16题目
+ * 三数之和
+ */
+public class ThreeSum {
+
+    public static void main(String[] args) {
+
+        threeSumClosest(new int[]{1,2,3,4,5,6,7},12);
+    }
+    public static int threeSumClosest(int[] nums, int target) {
+        // 排序
+        Arrays.sort(nums);
+        int closestNum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int l = i + 1, r = nums.length - 1;
+            while (l < r){
+                int threeSum = nums[l] + nums[r] + nums[i];
+                if (Math.abs(threeSum - target) < Math.abs(closestNum - target)) {
+                    closestNum = threeSum;
+                }
+                if (threeSum > target) {
+                    r--;
+                } else if (threeSum < target) {
+                    l++;
+                } else {
+                    // 如果已经等于target的话, 肯定是最接近的
+                    return target;
+                }
+
+            }
+
+        }
+
+        return closestNum;
+    }
+}
