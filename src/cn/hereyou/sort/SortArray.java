58758d46b3958f4d271eb6dce41420da56a7cff1
wuhuipeng <2252436103@qq.com>
2023-04-26 07:51:32 +0800

    提交冒泡排序和选择排序

---
+++ src/cn/hereyou/sort/SortArray.java
@@ -0,0 +1,34 @@
+package cn.hereyou.sort;
+
+
+import java.util.Arrays;
+
+/**
+ *  912. 排序数组
+ *  冒泡排序法
+ */
+public class SortArray {
+
+    public static void main(String[] args) {
+        int[] arr=sortArrar(new int[] {23,12,43,1,21,342,3,1,12});
+        System.out.println(Arrays.toString(arr));
+    }
+    public static   int[] sortArrar(int [] arr){
+
+        for (int i = 0; i <= arr.length - 1; i++) {
+            for (int j = 0; j <= arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int  temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+
+            }
+        }
+        return arr;
+
+    }
+
+
+
+}
